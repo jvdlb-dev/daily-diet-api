8ebddf8373404e5ce597d7c712e391f9c26bff3c
jvdlb6 <jvdlb6@gmail.com>
2026-07-10 23:10:13 -0300

    register user and meal routes in the Fastify application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import { userRoutes } from './routes/users'
+import { mealRoutes } from './routes/meals'
 
 export const app = fastify()
 
 app.register(cookie)
 
-// app.register(userRoutes, { prefix: 'users' })
-// app.register(mealRoutes, { prefix: 'meals' })
+app.register(userRoutes, { prefix: 'users' })
+app.register(mealRoutes, { prefix: 'meals' })
